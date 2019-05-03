73df238dc09a6b1ea44b1c85d3e3f2b3e8f84303
Sebastian Valencia <sebastian.valencia@yuxiglobal.com>
2019-05-03 11:31:20 -0500

    [Feature] Create the App component inject into main div

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-// let's go!
+import React from "react";
+import {render} from "react-dom";
+import App from "./components/App";
+
+//Every single component is a class
+
+//Small component/class basic
+class StorePicker extends React.Component {
+    //what html element do I put in the DOM
+    render() {
+        //return JSX
+        return (
+            <p>Hello World!</p>
+        )
+    }
+}
+
+//Render to the page
+//render(<p>HEYYYY</p>, document.querySelector('#main'));
+render(<StorePicker />, document.querySelector('#main'));
