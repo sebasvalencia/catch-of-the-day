95e29094a86c9e26f03baf9a0da0cce59371c4ef
Sebastian <sebastian.valencian@gmail.com>
2019-05-07 18:33:38 -0500

    [Feature] Create the StorePicker component and import in index.js

---
+++ src/components/StorePicker.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class StorePicker extends React.Component {
+    //what html element do I put in the DOM
+    render() {
+        //return JSX
+        return (
+            <p>Hello World!</p>
+        )
+    }
+}
+
+export default StorePicker;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
 import React from "react";
 import {render} from "react-dom";
 import App from "./components/App";
-
+import StorePicker from "./components/StorePicker";
 //Every single component is a class
 
 //Small component/class basic
-class StorePicker extends React.Component {
-    //what html element do I put in the DOM
-    render() {
-        //return JSX
-        return (
-            <p>Hello World!</p>
-        )
-    }
-}
+
 
 //Render to the page
 //render(<p>HEYYYY</p>, document.querySelector('#main'));
