33f80d94a512bd6dcb2802e065ed21fbd0428abb
Sebastian <sebastian.valencian@gmail.com>
2019-05-07 18:50:29 -0500

    [Feature] Add Fragment , create the form with input and button

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 class StorePicker extends React.Component {
-    //what html element do I put in the DOM
-    render() {
-        //return JSX
-        return (
-            <p>Hello World!</p>
-        )
-    }
+  //what html element do I put in the DOM
+  render() {
+    //return JSX
+    return (
+      // <p>Hello World!</p>
+      //   <React.Fragment>
+      <Fragment>
+        <p>Fish!</p>
+        <form className="store-selector">
+          {/* this is a comment in JSX */}
+          <h2>Please Enter A Store</h2>
+          <input type="text" placeholder="Store Name" required />
+          <button type='submit'>Visit Store ↪</button>
+        </form>
+      </Fragment>
+      //   </React.Fragment>
+    );
+  }
 }
 
 export default StorePicker;
