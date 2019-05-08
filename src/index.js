36fa5933937aa2d6ec437dee94dea7d0e401a74a
Sebastian <sebastian.valencian@gmail.com>
2019-05-07 19:43:41 -0500

    [Feature]Create Header,Inventory,Order comp. & add to App & render App

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from "react";
-import {render} from "react-dom";
+import { render } from "react-dom";
 import App from "./components/App";
 import StorePicker from "./components/StorePicker";
 
 import "./css/style.css";
 
-
 //Every single component is a class
 
 //Small component/class basic
 
-
 //Render to the page
 //render(<p>HEYYYY</p>, document.querySelector('#main'));
-render(<StorePicker />, document.querySelector('#main'));
+// render(<StorePicker />, document.querySelector('#main'));
+render(<App />, document.querySelector("#main"));
