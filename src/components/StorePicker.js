e783f46a9f4c1d9dce48b192128aca34bb1611c3
Sebastian <sebastian.valencian@gmail.com>
2019-05-08 20:20:17 -0500

    [Feature] add Ref and events for take values of inputs

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,10 +2,25 @@ import React, { Fragment } from "react";
 import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
-  handleClick = e => {
-    //alert('hey');
-    e.preventDefault();
-    console.log("handleClick");
+  // handleClick = e => {
+  //   //alert('hey');
+  //   e.preventDefault();
+  //   console.log("handleClick");
+  // };
+
+  //Create ref
+  myInput = React.createRef();
+
+  goToStore = event => {
+    console.log("Going to store");
+    //1. Stop the form from submitting
+    event.preventDefault();
+    //2. Get the text from that input
+    console.log(this);
+    const storeName = this.myInput.current.value;
+    console.log("storeName", storeName);
+    //3. Change the page to /store/whatever-they-entered
+    this.props.history.push(`/store/${storeName}`);
   };
 
   //what html element do I put in the DOM
@@ -16,12 +31,13 @@ class StorePicker extends React.Component {
       //   <React.Fragment>
       <Fragment>
         {/* <p>Fish!</p> */}
-        <form className="store-selector">
+        <form className="store-selector" onSubmit={this.goToStore}>
           {/* this is a comment in JSX */}
-          <button onClick={this.handleClick}>Click me!</button>
+          {/* <button onClick={this.handleClick}>Click me!</button> */}
           <h2>Please Enter A Store</h2>
           <input
             type="text"
+            ref={this.myInput}
             required
             placeholder="Store Name"
             defaultValue={getFunName()}
