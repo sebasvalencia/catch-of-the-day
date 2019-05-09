295952c07d07c25d604dea0cd99d69b24cc67310
Sebastian <sebastian.valencian@gmail.com>
2019-05-08 20:02:35 -0500

    [Feature]Test the onClick event

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,6 +2,12 @@ import React, { Fragment } from "react";
 import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+  handleClick = e => {
+    //alert('hey');
+    e.preventDefault();
+    console.log("handleClick");
+  };
+
   //what html element do I put in the DOM
   render() {
     //return JSX
@@ -12,6 +18,7 @@ class StorePicker extends React.Component {
         {/* <p>Fish!</p> */}
         <form className="store-selector">
           {/* this is a comment in JSX */}
+          <button onClick={this.handleClick}>Click me!</button>
           <h2>Please Enter A Store</h2>
           <input
             type="text"
